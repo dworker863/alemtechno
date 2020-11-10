eac8f77b4de392cc4a0b6ffbadbc18812c1fb2f2
dworker <dworker863@gmail.com>
2020-11-10 09:52:27 +0600

    Mobile. Add toggle button to Ourproject section

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-
   $('.third-button').on('click', function () {
     $('.animated-icon3').toggleClass('open');
   });
+
+  $('.btn-show').click(function () {
+    $('.row-show').toggleClass('d-none');
+  })
 });
